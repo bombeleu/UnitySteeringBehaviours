1aa75c8938ab4645a6ec7fcef65837e4b4c9f475
Kevin H <harriskevin123@gmail.com>
2014-04-29 17:29:33 +0100

    bomb

--- Assets/Scripts/Attack304.cs
+++ Assets/Scripts/Attack304.cs
@@ -2,48 +2,69 @@
 using System.Collections;
 
 public class Attack304 : MonoBehaviour {
-	public GameObject fireRound,fireRound1,fireRound2,fireRound3,fireRound4,fireRound5,fireRoundMissile1,fireRoundMissile2, fire;
+	public GameObject fireRound,fireRound1,fireRound2,fireRound3,fireRound4,fireRound5,fireRoundMissile1,
+	fireRoundMissile2, fire, lazerRound,missile,missileRound,rounds2;
 	GameObject enemyShip1,enemyShip2,enemyShip3,enemyShip4;
 	Transform target;
 	
 	GameObject mainCamera;
 	movement startBattle;
+
 	//collideWithShield collide;
 	// Use this for initialization
 	void Start () {
-		fireRound = GameObject.Find("cannon1");
-		fireRound1 = GameObject.Find("cannon2");
-		fireRound2 = GameObject.Find("cannon3");
-		fireRound3 = GameObject.Find("cannon4");
-		fireRound4 = GameObject.Find("cannon5");
-		fireRound5 = GameObject.Find("cannon6");
-
-		fireRoundMissile1 = GameObject.Find("missile1");
-		fireRoundMissile2 = GameObject.Find("missile2");
-
 		enemyShip1 = GameObject.FindGameObjectWithTag("Ori Mothership - 1");
 		enemyShip2 = GameObject.FindGameObjectWithTag("Ori Mothership - 2");
 		enemyShip3 = GameObject.FindGameObjectWithTag("Ori Mothership - 3");
 		enemyShip4 = GameObject.FindGameObjectWithTag("Ori Mothership - 4");
-		
+
 		mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-		startBattle = mainCamera.GetComponent<movement>(); 
+		startBattle = mainCamera.GetComponent<movement>();
 	}
-	bool up = false;
+	bool lazer = false,missileLaunch = false,
+	once = false,lazerVolley=false,lazerVolley1=false,
+	missileLaunch1 = false;
+	public bool move304camera =false;
+	float countForMissile = 0, roundsTheSecond=0;
+	int counter = 0;
 	// Update is called once per frame
 	void Update () {
-		Debug.Log(fire.name+ "     "+startBattle.commence);
-
 		if((fire!=null)&&(startBattle.commence)){
-			if(!up){
-				fire=Instantiate(fire,fireRound.transform.position, Quaternion.identity) as GameObject;
-				fire.tag="Fire";
-				up = true;
-				//fire.tag = "Fire";
+			if(!lazer){
+				lazerRound=Instantiate(fire,fireRound.transform.position, Quaternion.identity) as GameObject;
+				lazerRound.tag="Fire";
+				lazer = true;
 			}
-			fire.transform.position = Vector3.MoveTowards(fire.transform.position,
-			                                              enemyShip1.transform.position,
-			                                              100);
+			if(countForMissile <=1)
+				countForMissile += 0.05f;
+			if((countForMissile >=0.9f)&&(!once)){
+				missileLaunch = true;
+				lazerVolley = true;
+				once = true;
+			}
+			if(missileLaunch){
+				missileRound = Instantiate(missile,fireRoundMissile1.transform.position, Quaternion.identity) as GameObject;
+				missileLaunch = false;
+			}
+			if(lazerVolley){
+				rounds2= Instantiate(fire,fireRound1.transform.position, Quaternion.identity) as GameObject;
+				if(counter<=3){
+					if(Vector3.Distance(rounds2.transform.position,fireRound1.transform.position)>=25){
+						rounds2= Instantiate(fire,fireRound1.transform.position, Quaternion.identity) as GameObject;
+					}
+				}
+				counter +=1;
+				if(counter >=3){
+					lazerVolley = false;
+					counter=0;
+				}
+			}
+			if(lazerVolley==false){
+				//lazerVolley=false;
+				move304camera = true;
+				lazerVolley1=true;
+				missileLaunch1 = true;
+			}
 		}
 	}
 }
--- Assets/Scripts/collideWithShield.cs
+++ Assets/Scripts/collideWithShield.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class collideWithShield : MonoBehaviour {
 	public GameObject collision;
 	GameObject ship;
+	GameObject game;
 	// Use this for initialization
 	void Start () {
+		game = this.gameObject;
 		ship = GameObject.Find("Ori Mothership - 1");
 	}
-	
+	bool bomb = false;
 	// Update is called once per frame
 	void Update () {
-
-		if(Vector3.Distance(ship.transform.position,transform.position)<150){
-			Vector3 shield = transform.position;
-			Destroy(this.gameObject);
+		if(Vector3.Distance(ship.transform.position,transform.position)<110){
+			Vector3 shield = new Vector3(ship.transform.position.x,ship.transform.position.y,(ship.transform.position.z-100));
 			GameObject shieldHit = Instantiate(collision,shield, Quaternion.identity) as GameObject;
+			if(game!=null){
+				transform.position = new Vector3(transform.position.x,transform.position.y,(ship.transform.position.z-100));
+				Destroy(game);
+			}
+		}else{
+			transform.position = Vector3.MoveTowards(transform.position,ship.transform.position,25);
 		}
-	
 	}
-
 }
